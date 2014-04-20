d15a5d0e29902ce4e5627587ef1cc1814d29d143
Jason Goldberger <jlgoldb2@asu.edu>
2014-04-20 11:44:28 -0700

    added genFuzzies and genStrings funcs

--- fuzzyexp.js
+++ fuzzyexp.js
@@ -1,32 +1,51 @@
 
 
 var FuzzyExp = function(regex, options){
-  this.pattern        = new RegExp(regex);
-  this.options        = options;
-  
-  var getAtoms = function(){
-    var atoms1        = [];
-    var patternString = this.pattern.toString();
-    
+  this.regex                = regex;
+  this.patternRaw           = regex.toString();
+  this.pattern              = this.patternRaw.slice(1,this.patternRaw.length-1);
+  this.pArray               = this.pattern.split("");
+  this.fuzzies              = {};
 
+
+  var genFuzzies = function(stringy){
+    var fuzzyHolder,
+        fuzzies       = [];
+
+    for (var i = 0; i < stringy.pArray.length; i++){
+      fuzzyHolder = stringy.pArray.slice(0,i) + ['.'] + stringy.pArray.slice(i,stringy.pArray.length);
+      fuzzyHolder = fuzzyHolder.split("");
+      fuzzies.push(fuzzyHolder);
+    }
+    debugger;
+    return fuzzies;
   };
 
-  var splitRegEx = function(stringy, splitter){
-    var outStringArray = [];
-    outStringArray.push(stringy.split(splitter));
-    return outStringArray;
+  var genStrings = function(arraything){
+
+    for (i in arraything) {
+      arraything[i] = arraything[i];
+    }
   };
 
-  this.pattern.atoms = getAtoms();
 
+  this.fuzzies.arrays      = genFuzzies(this);
+  this.fuzzies.strings     = genStrings(this.fuzzies.arrays);
 
-
-  this.search = function(inString, minimumProb){
-    var outStringArray = [];
-  };
+/*
+  this.splitters = {};
+  this.splitters.parentheses      = ['(',')'];
+  this.splitters.curlyBrackets    = ['{','}'];
+  this.splitters.squareBrackets   = ['[',']'];
+  this.splitters.forwardSlashes   = ['/'];
+  this.splitters.backSlashes      = ['\\']; //does this work?
+  this.splitters.or               = ['|'];
+  this.splitters.not              = ['^'];
+  this.splitters.dash             = ['-'];
+*/
 
 
 };
 
-var jason = new FuzzyExp(/jason/);
+var jason = new FuzzyExp(/jason/, '');
 debugger;
